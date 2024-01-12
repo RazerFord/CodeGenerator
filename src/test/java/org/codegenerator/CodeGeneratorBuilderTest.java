cb2b6b5a5488a68982de8918f3c06a65ae33e418
RazerFord <desk10567@gmail.com>
2024-01-12 17:27:05 +0300

    feat: added tests for builder

---
+++ src/test/java/org/codegenerator/CodeGeneratorBuilderTest.java
@@ -0,0 +1,16 @@
+package org.codegenerator;
+
+import org.junit.jupiter.api.Test;
+
+public class CodeGeneratorBuilderTest {
+    private static final String OUTPUT_DIRECTORY = "./";
+    private static final String PACKAGE_NAME = "generatedclass";
+    private static final String METHOD_NAME = "generate";
+    private static final String CLASS_PATH_PREFIX = "./generatedclass/";
+    private static final String CLASS_NAME_PREFIX = "generatedclass.";
+
+    @Test
+    public void userBuilderTest() {
+        final String generatedClassName = "GeneratedUserClass";
+    }
+}
