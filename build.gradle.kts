6d9e188159f4822967fafddbfcdd3eaebdbc2dc7
RazerFord <desk10567@gmail.com>
2024-02-13 10:59:44 +0300

    refactoring: refactored build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,26 +3,31 @@ plugins {
 }
 
 group = "org.src"
-version = "1.0-SNAPSHOT"
+version = "0.2"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    val commonsLang3Version = "3.14.0"
     val jacodbVersion = "1.4.4"
+    val cloningVersion = "1.10.3"
+    val jbAnnotationsVersion = "24.0.0"
+    val javapoetVersion = "1.13.0"
+    val junitVersion = "5.9.1"
 
-    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-lang3:$commonsLang3Version")
 
-    implementation(group = "org.jacodb", name = "jacodb-api", version = jacodbVersion)
-    implementation(group = "org.jacodb", name = "jacodb-core", version = jacodbVersion)
-    implementation(group = "org.jacodb", name = "jacodb-analysis", version = jacodbVersion)
+    implementation("org.jacodb:jacodb-api:$jacodbVersion")
+    implementation("org.jacodb:jacodb-core:$jacodbVersion")
+    implementation("org.jacodb:jacodb-analysis:$jacodbVersion")
 
-    implementation("io.github.kostaskougios:cloning:1.10.3")
+    implementation("io.github.kostaskougios:cloning:$cloningVersion")
 
-    implementation("org.jetbrains:annotations:24.0.0")
-    implementation("com.squareup:javapoet:1.13.0")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    implementation("org.jetbrains:annotations:$jbAnnotationsVersion")
+    implementation("com.squareup:javapoet:$javapoetVersion")
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
