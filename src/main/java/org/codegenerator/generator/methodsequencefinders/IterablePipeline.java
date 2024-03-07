07c6e376d1cf169019e77c6b0b307531f9bbcb24
RazerFord <desk10567@gmail.com>
2024-03-08 01:14:48 +0300

    fix: working version of the iterator

--- src/main/java/org/codegenerator/generator/methodsequencefinders/IterablePipeline.java
+++ src/main/java/org/codegenerator/generator/methodsequencefinders/IterablePipeline.java
@@ -53,8 +53,7 @@ public class IterablePipeline implements Iterable<History<Executable>> {
         private final Map<Integer, Class<?>> indexToCreator = new HashMap<>();
         private final Set<Class<?>> doNotCache = new HashSet<>();
         private final Deque<IndexedWrapper<RangeResult>> stack = new ArrayDeque<>();
-        private final Deque<IndexedWrapper<RangeResult>> allRanges = new ArrayDeque<>();
-        private final Deque<IndexedWrapperResult> found = new ArrayDeque<>();
+        private final Deque<IndexedWrapper<RangeResult>> ranges = new ArrayDeque<>();
         private final History<Executable> history = new History<>();
         private int lastIndex = -1;
         private final TargetObject targetObject;
@@ -75,26 +74,38 @@ public class IterablePipeline implements Iterable<History<Executable>> {
             if (lastIndex >= iterable.finderCreators.size()) {
                 return false;
             }
-            if (!allRanges.isEmpty() || nextFinder(lastIndex + 1)) {
-                while (!stack.isEmpty() && allRanges.getLast().index <= stack.getLast().index) {
+            if (!ranges.isEmpty() || nextFinder(lastIndex + 1)) {
+                while (!stack.isEmpty() && ranges.getLast().index <= stack.getLast().index) {
                     stack.pollLast();
                 }
-                IndexedWrapper<RangeResult> last = allRanges.pollLast();
+                IndexedWrapper<RangeResult> last = ranges.getLast();
+                if (last.indexOfLastFound >= iterable.finderCreators.size() ||
+                        compare(last.value.getTo().getObject()) == 0) {
+                    ranges.pollLast();
+                }
                 stack.addLast(last);
                 while (compare(last.value.getTo().getObject()) != 0) {
                     RangeObject rangeObject = new RangeObject(last.value.getTo(), targetObject);
-                    IndexedWrapper<RangeResultFinding> indexed = findInternal(rangeObject, last.index + 1);
+                    IndexedWrapper<RangeResultFinding> indexed = findInternal(rangeObject, last.indexOfLastFound + 1);
 
                     if (indexed == null) {
+                        ranges.pollLast();
+                        last.indexOfLastFound = iterable.finderCreators.size();
                         return !stack.isEmpty();
                     }
+                    last.indexOfLastFound = indexed.index;
 
-                    indexed.value.getRanges().forEach(it -> allRanges.addLast(new IndexedWrapper<>(indexed.index, it)));
+                    indexed.value.getRanges().forEach(it -> ranges.addLast(new IndexedWrapper<>(indexed.index, it)));
 
-                    if (allRanges.isEmpty() || indexed.value.getRanges().isEmpty()) {
+                    if (ranges.isEmpty() ||
+                            indexed.value.getRanges().isEmpty()) {
                         return !stack.isEmpty();
                     }
-                    last = allRanges.pollLast();
+                    last = ranges.getLast();
+                    if (last.indexOfLastFound >= iterable.finderCreators.size() ||
+                            compare(last.value.getTo().getObject()) == 0) {
+                        ranges.pollLast();
+                    }
 
                     stack.addLast(last);
                 }
@@ -104,20 +115,10 @@ public class IterablePipeline implements Iterable<History<Executable>> {
 
         @Override
         public @Nullable History<Executable> next() {
-            Iterator<IndexedWrapper<RangeResult>> iterator = stack.descendingIterator();
+            HistoryObject<Executable> node = createHistoryObject();
 
-            HistoryObject<Executable> node = null;
-            while (iterator.hasNext()) {
-                IndexedWrapper<RangeResult> indexed = iterator.next();
-                Object target = indexed.value.getTo().getObject();
-
-                node = new HistoryObject<>(target,
-                        toHistoryCalls(history, indexed.value),
-                        indexToCreator.get(indexed.index),
-                        node
-                );
-            }
             history.put(targetObject.getObject(), node);
+
             TargetObject to = stack.getLast().value.getTo();
             if (compare(to) != 0) {
                 useReflection(targetObject, to, history);
@@ -130,14 +131,19 @@ public class IterablePipeline implements Iterable<History<Executable>> {
             IndexedWrapper<RangeResultFinding> indexed = findInternal(targetObject, nextIndex);
 
             if (indexed != null) {
-                found.addLast(new IndexedWrapperResult(indexed));
                 lastIndex = indexed.index;
-                indexed.value.getSuspects().stream()
+
+                indexed.value
+                        .getSuspects()
+                        .stream()
                         .filter(s -> !history.contains(s))
                         .map(TargetObject::new)
                         .map(iterable.pipeline::findReflectionCalls)
                         .forEach(history::merge);
-                indexed.value.getRanges().forEach(it -> allRanges.addLast(new IndexedWrapper<>(indexed.index, it)));
+
+                indexed.value
+                        .getRanges()
+                        .forEach(it -> ranges.addLast(new IndexedWrapper<>(indexed.index, it)));
             } else {
                 lastIndex = iterable.finderCreators.size();
             }
@@ -226,6 +232,23 @@ public class IterablePipeline implements Iterable<History<Executable>> {
             return targetNode.diff(ClassFieldExtractor.extract(object));
         }
 
+        private HistoryObject<Executable> createHistoryObject() {
+            Iterator<IndexedWrapper<RangeResult>> iterator = stack.descendingIterator();
+
+            HistoryObject<Executable> node = null;
+            while (iterator.hasNext()) {
+                IndexedWrapper<RangeResult> indexed = iterator.next();
+                Object target = indexed.value.getTo().getObject();
+
+                node = new HistoryObject<>(target,
+                        toHistoryCalls(history, indexed.value),
+                        indexToCreator.get(indexed.index),
+                        node
+                );
+            }
+            return node;
+        }
+
         private void useReflection(
                 TargetObject expected,
                 TargetObject actual,
@@ -247,11 +270,6 @@ public class IterablePipeline implements Iterable<History<Executable>> {
         }
     }
 
-    private static <K, V> void putIfNotExists(@NotNull Map<K, List<V>> map, K k, V v) {
-        List<V> list = map.computeIfAbsent(k, k1 -> new ArrayList<>());
-        list.add(v);
-    }
-
     private static void logging(@Nullable MethodSequenceFinder finder) {
         if (finder != null) {
             String className = finder.getClass().getName();
@@ -266,46 +284,15 @@ public class IterablePipeline implements Iterable<History<Executable>> {
         }
     }
 
-    private static class IndexedWrapperResult implements Iterator<RangeResult> {
-        private boolean isFirst = true;
-        private boolean isFound = false;
-        private final int index;
-        private final RangeResultFinding result;
-        private final Iterator<RangeResult> iterator;
-
-        public IndexedWrapperResult(@NotNull IndexedWrapper<RangeResultFinding> result) {
-            this.index = result.index;
-            this.result = result.value;
-            iterator = result.value.getRanges().iterator();
-        }
-
-        @Override
-        public boolean hasNext() {
-            return iterator.hasNext();
-        }
-
-        @Override
-        public RangeResult next() {
-            isFirst = false;
-            return iterator.next();
-        }
-
-        public boolean isFound() {
-            return isFound;
-        }
-
-        public boolean isFirst() {
-            return isFirst;
-        }
-    }
-
     private static class IndexedWrapper<T> {
         private final int index;
         private final T value;
+        private int indexOfLastFound;
 
         public IndexedWrapper(int index, T value) {
             this.index = index;
             this.value = value;
+            this.indexOfLastFound = index;
         }
     }
 }
